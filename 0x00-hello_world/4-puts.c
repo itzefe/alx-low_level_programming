0469bae4ee546e2c8fc637fa238a798971ba42c2
itzefe <bern.owusuansah@gmail.com>
2022-12-08 15:04:46 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a line of a code using puts
+ * main -print a line of code usings
  * Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-i}
+}
