74c98155f2c5d6367324384bec7d9e57d4df2bd9
itzefe <bern.owusuansah@gmail.com>
2022-12-08 18:35:02 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a line of code using write
+ * main -print a line of code without puts or printf
  *
  * Return: 1
 */
 int main(void)
 {
 	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-	return (1)
+	return (1}
 }
