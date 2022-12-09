8ce33d964629ebfb60dbf8b80140909e2911266f
itzefe <bern.owusuansah@gmail.com>
2022-12-08 17:13:30 -0800

    grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * maim - print out sizes of data types in c
+ * Code by Efe
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf(“size of a char: %lu byte(S)\n”, (unsigned long)sizeof(a));
+	printf(“size of an int: %lu byte(S)\n”, (unsigned long)sizeof(b));
+	printf(“size of a long: %lu byte(S)\n”, (unsigned long)sizeof(c));
+	printf(“size of a long long int: %lu byte(S)\n”, (unsigned long)sizeof(d));
+	printf(“size of a float: %lu byte(S)\n”, (unsigned long)sizeof(f));
+	return (0)
+}
