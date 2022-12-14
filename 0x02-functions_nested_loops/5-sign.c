790d96d222f09e5ebc743f52507335c4ec0715bf
itzefe <bern.owusuansah@gmail.com>
2022-12-14 08:58:06 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - print the size of a number
  * @n: thr num to be checked
  * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ *	0 and prints 0 if n is zero
+ *	-1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
